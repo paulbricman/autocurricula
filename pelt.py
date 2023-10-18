056c1dd68928815fc711a43028e4ad071338e643
paulbricman <paulbricman@protonmail.com>
2023-10-18 15:03:12 +0300

    feat: impl preliminary adapter tests

--- pelt.py
+++ pelt.py
@@ -1,26 +1,27 @@
-from pelt.utils import to_actual_model_tokenizer
-from pelt.training import update
-from pelt.defaults import league_entry, league_match, default_config
+from pelt.training import (
+    update,
+    populate_with_entrant_adapters,
+    peft_model_tok,
+)
+from pelt.operators import league_entry, league_match
+from pelt.defaults import default_config
 
 
 def league(
-    model,
-    play,
-    match=league_match,
-    entry=league_entry,
-    config=default_config(),
-    tokenizer=None,
+    model_name, play, match=league_match, entry=league_entry, config=default_config()
 ):
-    if isinstance(model, str):
-        model, tokenizer = to_actual_model_tokenizer(model)
+    model, tokenizer = peft_model_tok(model_name, config)
 
     league = []
     for generation in range(config["league"]["n_generations"]):
-        league += entry(generation, config)
+        entrants = entry(model, generation, config)
+        populate_with_entrant_adapters(model, entrants, config)
         matches = match(league, generation, config)
 
         for round in range(config["league"]["n_rounds"]):
-            evals_and_history = [play(match, tokenizer, config) for match in matches]
+            evals_and_history = [
+                play(model, match, tokenizer, config) for match in matches
+            ]
             league = update(league, matches, evals_and_history, config)
 
     return league
